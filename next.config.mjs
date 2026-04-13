804f4bcfec40458325282b7df690d37cec2e78a1
vdemon1 <vdemon1@users.noreply.github.com>
2026-04-13 16:27:15 -0300

    Configurar GitHub Pages Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = ''
+if (isGithubActions) {
+  // Substitua 'portfolio' se mudar o nome do repositório no futuro
+  repo = '/portfolio'
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: repo,
+  assetPrefix: repo,
   typescript: {
     ignoreBuildErrors: true,
   },
